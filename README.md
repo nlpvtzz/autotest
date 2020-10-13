6f4bcb71bd25b4946ccfe37c39413e1b9bb5eefe
Neil Paiva Tizzo <nlpvtzz@gmail.com>
2020-10-13 12:21:25 -0300

    não para mais no 1o erro

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 Este programa, autotest.c, realiza o teste automático de outros programas. 
-Isto é, ele verifica se a saída de um programa é igual à uma saída pré-definida.
+Ele verifica se a saída de um programa é igual à uma saída pré-definida.
 Para isto é necessário criar os aquivos de entreda (input_i.txt) e de saída (output_i.txt), onde i é o sufixo dos respectivos arquivos. 
 Pode-se ter quantos arquivos de entrada e saída quantos forem necessários, sempre aos pares: para cada arquivo de entrada deve-se ter um de saída.
 Por exemplo, vamos supor que um programa recebe dois números e calcaula a soma destes dois números. 
 Então um arquivo de entrada teria duas duas linhas: uma para cada numero e deveria se chamar input_1.txt.
 O seu respectivo arquivo de saida, output_1.txt, deve ter uma linha com a soma dos dois valores descritos no arquivo input_1.txt.
-Por exemplo:
+Pode-se ter quantos arquivos de testes quantos forem necessários, apenas deve-se mudar o sufixo i de cada arquivo. sendo i um inteiro incremental 
+començando de 1.
+
+Exemplo: arquivos que testam um programa que recebe dois números e fornece como saída a soma desdes dois números.
 
 a)
 Arquivo de entrada input_1.txt
@@ -24,8 +27,10 @@ Arquivo de saida output_2.txt
 Obs.: as saídas são comparadas caracter a caracter, portanto, espaços a mais ou a menos em uma string acusará falha no teste.
 
 Resumo do modo de usar:
-1) Em uma pasta qualquer copie o executável do autotest.c (autotest.exe)
-2) Crie os arquivos de entrada (input_i.txt) e os respectivos de saída (output_i.txt)
-3) Dentro desta pasta, crie uma subpasta chamada bin
-4) Dentro da subpasta bin, crie uma subpasta chamada Debug
-5) Na pasta raiz do autotest, execute o seguinte comando DOS: autotest nome_programa_a_ser_testado
+
+1) Em uma pasta qualquer copie o executável do autotest (autotest.exe)
+
+2) Crie os arquivos de entrada (input_i.txt) e os respectivos de saída (output_i.txt), 
+sendo i um numero inteiro comencando de zero e crescente
+
+3) Execute o seguinte comando DOS: autotest caminho\nome_programa_a_ser_testado.exe
--- autotest.c
+++ autotest.c
@@ -9,52 +9,65 @@
 
 int outputCompare (char *, char *, char *);
 int buffer2Row (char *, int, char [][OUTPUT_ROW_SIZE]);
-int CommandMake (char *, char *, char *, char *);
+int commandMaker (char *, char *, char *, char *);
 int fileRead (char *, char *);
 int stringCount(char [], int, char [], int);
-int testCount (char [], int);
+int testCount (char *, char *, int);
 long fileSize (char *);
 
 int main (int argc, char *argv[]) {
-    char workDir [] = "..\\bin\\Debug\\";
-    char testDir [] = ".\\";
+    char workDir [FILE_NAME_SIZE];
     char programName[PROGRAM_NAME_SIZE];
     char programFullName[2*PROGRAM_NAME_SIZE];
     char command[COMMAND_SIZE];
     char inputFileName[FILE_NAME_SIZE];
     char outputFileName[FILE_NAME_SIZE];
     char testFileName[FILE_NAME_SIZE];
-    int resp;
+    int fails=0;
 
-    sprintf(programName, "%s", argv[1]);
-    sprintf (programFullName, "\"%s%s\"", workDir, programName);
+    if (argc==1) {
+        printf("Program to test: ");
+        fflush(stdin);
+        gets(programFullName);
+        strcpy(programName, programFullName);
+
+        printf("Directory of tests: ");
+        fflush(stdin);
+        gets(workDir);
+
+    } else {
+        sprintf(programName, "%s", argv[1]);
+        strcpy (workDir, ".\\");
+        sprintf (programFullName, "\"%s%s\"", workDir, programName);
+    }
 
     //printf("\nDir\n%s\n\n", workDir);
     //printf("Program\n%s\n\n", programName);
     //printf("Full name\n%s\n\n", programFullName);
 
-    int qtInput = testCount("input_", sizeof("input_")-1);
+    int qtInput = testCount("input_", workDir, sizeof("input_")-1);
     if(qtInput == 0){
         printf("Error: no test files are found\n");
         return -1;
     }
-    int qtOutput = testCount("output_", sizeof("output_")-1);
+    int qtOutput = testCount("output_", workDir, sizeof("output_")-1);
     if (qtInput != qtOutput) {
-        printf("Error: the number of input and oiutput files are not the same\n");
+        printf("Error: the number of input and output files are not the same\n");
         return -1;
     }
 
     int qtTest = qtInput;
     for (int i=1; i<=qtTest; i++) {
         // make the input and output files names
-        sprintf(inputFileName, "%sinput_%d.txt", testDir, i);
-        sprintf(outputFileName, "%soutput_%d.txt", testDir, i);
-        sprintf(testFileName, "%soutputTest_%d.txt", testDir, i);
+        //printf("Work dir: %s\n", workDir);
+        sprintf(inputFileName, "%s\\input_%d.txt", workDir, i);
+        sprintf(testFileName, "%s\\outputTest_%d.txt", workDir, i);
+        sprintf(outputFileName, "%s\\output_%d.txt", workDir, i);
 
         // make the execution command name
         strcpy(command, "");
-        CommandMake(inputFileName, testFileName, programFullName, command);
-        //printf ("Command: \n%s\n", command);
+        commandMaker(inputFileName, testFileName, programFullName, command);
+        //printf ("Command:\n%s\n", command);
         int systemReturn = system (command);
         if (systemReturn!=0) {
             printf("\nError trying to execute the command:\n%s\n", command);
@@ -62,11 +75,11 @@ int main (int argc, char *argv[]) {
         }
 
         printf("\nTest %d ", i);
-        resp = outputCompare(inputFileName, outputFileName, testFileName);
-        if (resp == -1)
-            break;
+        fails += outputCompare(inputFileName, outputFileName, testFileName);
+        //if (fails  == -1)
+          //  break;
     }
-    if(resp==-1){
+    if(fails){
         printf("\n---------- Resultado Final: FALHA -------------\n\n");
     } else {
         printf("\n---------- Resultado Final: SUCESSO -----------\n\n");
@@ -74,6 +87,64 @@ int main (int argc, char *argv[]) {
     return 0;
 }
 
+int outputCompare (char *inputFileName, char *outputFileName, char *testOutputFileName){
+    int fails = 0;
+
+    int inputFilesize = fileSize(inputFileName);
+    if (inputFilesize<0) return -1;
+    char* inputBuffer = (char *) malloc(inputFilesize+1 * sizeof(char));
+
+    int outputFileSize = fileSize(outputFileName);
+    if (outputFileSize<0) return -1;
+    char* outputBuffer = (char *) malloc(outputFileSize+1 * sizeof(char));
+
+    int testFileSize = fileSize(testOutputFileName);
+    if (testFileSize<0) return -1;
+    char* testBuffer = (char *) malloc(testFileSize+1 * sizeof(char));
+
+    int r = fileRead(inputFileName, inputBuffer);
+    if (r!=0) return -1;
+    inputBuffer[inputFilesize]='\0';
+    //printf ("\nInput buffer\n%s\n", inputBuffer);
+
+    r = fileRead(outputFileName, outputBuffer);
+    if (r!=0) return -1;
+    outputBuffer[outputFileSize]='\0';
+    //printf ("\nOutput buffer\n%s\n", outputBuffer);
+
+    r = fileRead(testOutputFileName, testBuffer);
+    if (r!=0) return -1;
+    testBuffer[testFileSize]='\0';
+    //printf ("\nTest buffer\n%s\n", testBuffer);
+
+    if (outputFileSize != testFileSize) {
+        printf("-------------> Falha: saidas possuem tamanhos diferentes\n");
+        fails++;
+        //return -1;
+    } else {
+        for (int i=0; i<=outputFileSize; i++) {
+            if(outputBuffer[i] != testBuffer[i]) {
+                fails++;
+                printf("--> Falha: saida realizada diferente da esperada\n");
+                break;
+            }
+        }
+    }
+
+    if (fails) {
+        printf("--> Entrada:\n%s\n", inputBuffer);
+        printf("--> Saida Realizada:\n%s\n", testBuffer);
+        printf("--> Saida Esperada:\n%s\n", outputBuffer);
+
+        return fails;
+    } else {
+        printf ("  -->  ok\n");
+    }
+
+    return fails;
+}
+
+/*
 int outputCompare (char *inputFileName, char *outputFileName, char *testOutputFileName){
     int inputFilesize = fileSize(inputFileName);
     if (inputFilesize<0) return -1;
@@ -90,14 +161,17 @@ int outputCompare (char *inputFileName, char *outputFileName, char *testOutputFi
     int r = fileRead(inputFileName, inputBuffer);
     if (r!=0) return -1;
     inputBuffer[inputFilesize]='\0';
-
-    r = fileRead(testOutputFileName, testBuffer);
-    if (r!=0) return -1;
-    testBuffer[testFileSize]='\0';
+    printf ("\nInput buffer\n%s\n", inputBuffer);
 
     r = fileRead(outputFileName, outputBuffer);
     if (r!=0) return -1;
     outputBuffer[outputFileSize]='\0';
+    printf ("\nOutput buffer\n%s\n", outputBuffer);
+
+    r = fileRead(testOutputFileName, testBuffer);
+    if (r!=0) return -1;
+    testBuffer[testFileSize]='\0';
+    printf ("\nTest buffer\n%s\n", testBuffer);
 
     char rowIn[10][OUTPUT_ROW_SIZE];
     buffer2Row(inputBuffer, inputFilesize, rowIn);
@@ -127,6 +201,7 @@ int outputCompare (char *inputFileName, char *outputFileName, char *testOutputFi
     }
     return 0;
 }
+*/
 
 int buffer2Row (char *buffer, int size, char rows[][OUTPUT_ROW_SIZE]) {
     int c=0;
@@ -148,7 +223,7 @@ int buffer2Row (char *buffer, int size, char rows[][OUTPUT_ROW_SIZE]) {
     return l;
 }
 
-int CommandMake (char *input, char *output, char *arquivo, char *text) {
+int commandMaker (char *input, char *output, char *arquivo, char *text) {
     strcat (text, arquivo);
     strcat (text, " < ");
     strcat (text, input);
@@ -207,15 +282,21 @@ int fileRead (char *arquivoName, char *buffer) {
     return 0;
 }
 
-int testCount (char fileName[], int tam) {
+int testCount (char fileName[], char dir[], int tam) {
     int ocorrs = 0;
-    system ("dir /b > qtTest.txt");
-    int testFileSize = fileSize("qtTest.txt");
+    char command [COMMAND_SIZE];
+    char fileFullName [FILE_NAME_SIZE];
+
+    sprintf(fileFullName, "%s\\qtTest.txt", dir);
+    sprintf(command, "dir %s /b >> %s", dir, fileFullName);
+    system (command);
+
+    int testFileSize = fileSize(fileFullName);
     if (testFileSize == -1) return -1;
 
     char* buffer = (char *) malloc(testFileSize+1 * sizeof(char));
 
-    int r = fileRead("qtTest.txt", buffer);
+    int r = fileRead(fileFullName, buffer);
     if (r!=0) return -1;
     buffer[testFileSize]='\0';
 
@@ -225,7 +306,10 @@ int testCount (char fileName[], int tam) {
     for (int i= 0; i<l; i++)   {
         ocorrs += stringCount(rows[i], sizeof(rows[i]), fileName, tam);
     }
-    system ("del qtTest.txt");
+    //printf("Qt arquivos de teste: %d\n", ocorrs);
+
+    sprintf(command, "del %s", fileFullName);
+    system (command);
 
     return ocorrs;
 }
